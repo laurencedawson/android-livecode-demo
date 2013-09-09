83d58023acd3760a22f245c61c616d8d72231828
Laurence Dawson <loljdawson@gmail.com>
2013-09-09 22:15:10 +0100

    Create a Response.ErrorListener to listen for any errors returned by the previously created JsonObjectRequest.

--- src/com/laurencedawson/livecode/MainActivity.java
+++ src/com/laurencedawson/livecode/MainActivity.java
@@ -6,7 +6,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.volley.Response;
 import com.android.volley.Response.Listener;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 
 public class MainActivity extends Activity {
@@ -18,14 +20,29 @@ public class MainActivity extends Activity {
 
     // Create a simple JsonObjectRequest
     CustomApplication.requestQueue.add(new JsonObjectRequest(
-        "http://reddit.com/r/worldnews.json", null, new Listener<JSONObject>() {
+        "http://reddit.com/r/worldnews.json", null, 
+        
+        // Create a Response Listener object, the data returned by calling the aforementioned URL
+        // will be returned in a JSON object to this listener on the UI thread.
+        new Listener<JSONObject>() {
           
           @Override
           public void onResponse(JSONObject response) {
             Log.d(CustomApplication.TAG, response.toString());
           }
           
-        }, null));
+        }, 
+        
+        // Create a Response ErrorListener object, if anything goes wrong in the Request, this
+        // callback will be called
+        new Response.ErrorListener() {
+
+          @Override
+          public void onErrorResponse(VolleyError error) {
+            Log.e(CustomApplication.TAG, error.toString());
+          }
+          
+        }));
   }
 
 }
