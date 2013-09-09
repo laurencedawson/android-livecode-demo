c18563f699a454dfa2c2815de37797ca66ceb2f3
Laurence Dawson <loljdawson@gmail.com>
2013-09-09 21:21:46 +0100

    Create a static instance of RequestQueue within RequestQueue. This will be accessible throughout the app by calling CustomApplication.requestQueue.

--- src/com/laurencedawson/livecode/CustomApplication.java
+++ src/com/laurencedawson/livecode/CustomApplication.java
@@ -2,11 +2,23 @@ package com.laurencedawson.livecode;
 
 import android.app.Application;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class CustomApplication extends Application {
 
+  /**
+   * Create a static instance of the RequestQueue, this will be accessible throughout the app by
+   * calling CustomApplication.requestQueue
+   */
+  public static RequestQueue requestQueue;
+  
   @Override
   public void onCreate() {
     super.onCreate();
+    
+    // Create an instance of request queue
+    requestQueue = Volley.newRequestQueue(this);
   }
   
 }
