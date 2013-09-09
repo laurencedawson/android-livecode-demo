14f2dc4f32f176897339357f08ca197461169524
Laurence Dawson <loljdawson@gmail.com>
2013-09-09 21:14:01 +0100

    Create a custom Application and set the name to com.laurencedawson.livecode.CustomApplication in AndroidManifest.xml. This defines the custom Application for the package.

---
+++ src/com/laurencedawson/livecode/CustomApplication.java
@@ -0,0 +1,12 @@
+package com.laurencedawson.livecode;
+
+import android.app.Application;
+
+public class CustomApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+  
+}
