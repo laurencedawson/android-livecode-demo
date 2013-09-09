3499c4e33addb0dcd175b48895b15bd88fbb6165
Laurence Dawson <loljdawson@gmail.com>
2013-09-09 21:50:32 +0100

    Create a simple Volley Request using JsonObjectRequest to parse http://reddit.com/r/worldnews.json. Add the request to the previously defined RequestQueue in CustomApplication. Log the raw data returned.

--- src/com/laurencedawson/livecode/MainActivity.java
+++ src/com/laurencedawson/livecode/MainActivity.java
@@ -1,7 +1,13 @@
 package com.laurencedawson.livecode;
 
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Response.Listener;
+import com.android.volley.toolbox.JsonObjectRequest;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +15,17 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    // Create a simple JsonObjectRequest
+    CustomApplication.requestQueue.add(new JsonObjectRequest(
+        "http://reddit.com/r/worldnews.json", null, new Listener<JSONObject>() {
+          
+          @Override
+          public void onResponse(JSONObject response) {
+            Log.d(CustomApplication.TAG, response.toString());
+          }
+          
+        }, null));
   }
-  
+
 }
