7ffc9b30c620964f8a2bf7e71c2128a13f393a08
Laurence Dawson <loljdawson@gmail.com>
2013-09-09 21:46:39 +0100

    Add the permissions android.permission.INTERNET and android.permission.ACCESS_NETWORK_STATE to AndroidManifest.xml.

--- src/com/laurencedawson/livecode/CustomApplication.java
+++ src/com/laurencedawson/livecode/CustomApplication.java
@@ -6,10 +6,21 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
 public class CustomApplication extends Application {
+  
+  // Tag to be used with Log.d
+  public static final String TAG = "Livecode";
 
   /**
    * Create a static instance of the RequestQueue, this will be accessible throughout the app by
    * calling CustomApplication.requestQueue
+   * 
+   * 
+   * RequestQueue.java
+   * A request dispatch queue with a thread pool of dispatchers. Calling add(Request) will enqueue 
+   * the given Request for dispatch, resolving from either cache or network on a worker thread, 
+   * and then delivering a parsed response on the main thread.
+   * 
+   * http://afzalive.github.io/volley/com/android/volley/RequestQueue.html
    */
   public static RequestQueue requestQueue;
   
