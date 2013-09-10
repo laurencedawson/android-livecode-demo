c0e9729e8848baacfa6f9e3db4eb1ea387ba87a1
Laurence Dawson <loljdawson@gmail.com>
2013-09-10 11:15:44 +0100

    Created the class NewsItem to hold parsed news items from reddit. Currently contains two fields, title and author.

---
+++ src/com/laurencedawson/livecode/NewsItem.java
@@ -0,0 +1,13 @@
+package com.laurencedawson.livecode;
+
+public class NewsItem {
+
+  public String title;
+  public String author;
+  
+  public NewsItem(String title, String author){
+    this.title = title;
+    this.author = author;
+  }
+  
+}
